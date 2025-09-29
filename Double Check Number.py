7d4a0f8b280afd2070e48e3abc86339187817a80
wajihakanwalgit <vajeeha.kanwal25@gmail.com>
2025-09-29 21:18:01 +0500

    Add files via upload

---
+++ Double Check Number.py
@@ -0,0 +1,12 @@
+
+number  = int(input("Enter a number: "))
+
+
+double_number = number * 2
+print("Double of", number, "is", double_number)
+
+
+if double_number % 2 == 0:
+    print("The doubled number is Even")
+else:
+    print("The doubled number is Odd")
