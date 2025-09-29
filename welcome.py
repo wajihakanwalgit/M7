c9cd537350251edb307133e6bf07dc1353747b49
wajihakanwalgit <vajeeha.kanwal25@gmail.com>
2025-09-29 21:12:20 +0500

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -4,3 +4,28 @@ print(4)
 print("Hello world \n")
 print("keyword in python are")
 print(keyword.kwlist)
+// data types in python 
+name = "codingal"
+age = 12
+is_student = True
+weight = 21
+
+print("Name :", name)
+print("Data Type of Name is", type(name))
+print("Age :", age)
+print("Data Type of Age is", type(age))
+print("is_student :", is_student)
+print("Data Type of is_student is", type(is_student))
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
+
+
+
