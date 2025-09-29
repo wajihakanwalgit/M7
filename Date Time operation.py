e35169d63101179d5acd1767cc52906e53e959c3
wajihakanwalgit <vajeeha.kanwal25@gmail.com>
2025-09-29 21:19:00 +0500

    Add files via upload

---
+++ Date Time operation.py
@@ -0,0 +1,10 @@
+import datetime
+import calendar
+
+current_time = datetime.datetime.now()
+
+
+print("Current date and time:", current_time)
+
+print("\nFull calendar of the year 2021:\n")
+print(calendar.calendar(2021))
